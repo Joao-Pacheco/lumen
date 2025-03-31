ad5046fd2f67d6a9751b66ed2cd58007ffed331a
Joao Pacheco <juhkittopacheco@gmail.com>
2025-03-31 16:26:33 -0300

    feat: persist the last read day

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 "use client";
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import ProgressBar from "../components/ProgressBar";
 import dataBible from "../public/plan/plan365.json";
 import ReadingBox from "@/components/ReadingBox";
 import useStoreCurrentDate from "@/store/useStoreCurrentDate";
 import Footer from "@/components/Footer";
+import { getStorage, setStorage } from "@/lib/storage-handler";
+import { STORAGE_KEY } from "@/common/const";
 
 export default function Home() {
+  const [firstLoad, setFirstLoad] = useState(true);
   const [progress, setProgress] = useState<number>(0);
-  const { currentDate } = useStoreCurrentDate();
+  const { currentDate, setCurrentDate } = useStoreCurrentDate();
   const [dataToShow, setDataToShow] = useState({ month: "", day: "" });
   const [dailyText, setDailyText] = useState({
     Old: "",
@@ -17,34 +20,46 @@ export default function Home() {
     Proverbs: "",
   });
 
-  const transformDate = useCallback((date: Date) => {
+  function transformDate(date: Date) {
     const month = date.toLocaleString("pt-BR", { month: "long" });
     const day = String(date.getDate()).padStart(2, "0");
-    setDataToShow({ month, day });
     return { month, day };
-  }, []);
+  }
 
-  const findDailyText = useCallback(
-    (data: Date) => {
-      const newDate = transformDate(data);
-      return (dataBible as Plan365)[newDate.month][parseInt(newDate.day)];
-    },
-    [transformDate]
-  );
+  function findDailyText(date: Date) {
+    const { month, day } = transformDate(date);
+    return (
+      (dataBible as Plan365)[month]?.[parseInt(day)] ?? {
+        Old: "",
+        New: "",
+        Psalms: "",
+        Proverbs: "",
+      }
+    );
+  }
 
   function getDayOfYear(date: Date) {
     const start = new Date(date.getFullYear(), 0, 0);
     const diff = date.getTime() - start.getTime();
-    const oneDay = 1000 * 60 * 60 * 24;
-    return Math.floor(diff / oneDay);
+    return Math.floor(diff / (1000 * 60 * 60 * 24));
   }
 
   useEffect(() => {
+    if (firstLoad) {
+      const storedDate = new Date(getStorage(STORAGE_KEY) ?? currentDate);
+      setFirstLoad(false);
+      setCurrentDate(storedDate);
+    }
+
+    setDataToShow(transformDate(currentDate));
+    setDailyText(findDailyText(currentDate));
+
     const dayOfYear = getDayOfYear(currentDate);
     const totalDays = currentDate.getFullYear() % 4 === 0 ? 366 : 365;
     setProgress(Math.floor((dayOfYear / totalDays) * 100));
-    setDailyText(findDailyText(currentDate));
-  }, [currentDate, findDailyText]);
+
+    setStorage(STORAGE_KEY, currentDate.toString());
+  }, [currentDate]);
 
   return (
     <div className="p-5 flex flex-col items-center">
